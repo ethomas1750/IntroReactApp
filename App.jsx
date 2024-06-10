dd7ced2f992fea8200ef5bfb0f4a4c0adee51c0d
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 21:49:06 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,24 @@
+mport React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+import Body from './components/Body'
 
+
+
+export default function App() {
+ 
+  return (
+    <div>
+      <Header/>
+        <div style={{display: 'flex', width: '100vw', height: '60vh', marginTop: '20vh'}}>
+        <Sidebar/>
+        <Body/>
+        </div>
+
+        
+
+      <Footer/>
+    </div>
+  )
+}
